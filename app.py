8a75dc43f5abb41fe23388b2e694ea4088fb3389
JoaoADoja <joaoadojadias@gmail.com>
2024-01-25 14:19:20 -0300

    Primeira parte do projeto !

---
+++ app.py
@@ -0,0 +1,10 @@
+print('\n𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤\n ')
+
+print('1. Cadastar restaurante')
+print('2. Exibir restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção: {opcao_escolhida}')
---
+++ teste.py
@@ -0,0 +1,7 @@
+print("Python na Escola de Programação da Alura.")
+
+Nome = "João Augusto Doja Dias"
+Idade = 17
+
+print(f'Meu nome é {Nome} e eu tenho {Idade} anos !')
+
